f945f36d092674c1439198ef2a8fb1f9dc919d9b
Ozy <munkhtamir.g@outlook.com>
2022-07-09 00:11:48 +0800

    Header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 import "./header.css";
+import ME from "../../assets/profile.jpg";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Munkhtamir</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+      </div>
+    </header>
+  );
 }
