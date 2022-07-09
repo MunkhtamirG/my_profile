5b371fdb1fb879a3361b83ee5a5ee91db12826a3
Ozy <munkhtamir.g@outlook.com>
2022-07-10 00:58:59 +0800

    ADD footer & contact

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./footer.css";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
 
 export default function about() {
   return (
     <footer>
       <a href="#" className="footer_logo">
-        EGATOR
+        Portfolio
       </a>
-
       <ul className="permalinks">
         <li>
           <a href="#">Home</a>
@@ -25,6 +27,21 @@ export default function about() {
           <a href="#contact">Contact</a>
         </li>
       </ul>
+      <div className="footer_socials">
+        <a href="https://facebook.com">
+          <FaFacebookF />
+        </a>
+        <a href="https://instagram.com">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <FaTwitter />
+        </a>
+      </div>
+
+      <div className="footer_copyrights">
+        <small>&copy; Portfolio. All rights reserved.</small>
+      </div>
     </footer>
   );
 }
